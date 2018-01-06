92838e8406bc944ccd0ab448dc53481fcbc2c1d5
okki23 <okkisetyawan@gmail.com>
2018-01-06 15:42:30 +0700

    updatepertama

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+require 'PHPMailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
+
+// Konfigurasi SMTP
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'okkisetyawan@gmail.com';
+$mail->Password = 'Katerban_93';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+
+$mail->setFrom('okkisetyawan@contoh.com', 'Codingan');
+$mail->addReplyTo('okkisetyawan@contoh.com', 'Codingan');
+
+// Menambahkan penerima
+$mail->addAddress('okkisetyawan@gmail.com');
+
+// Menambahkan beberapa penerima
+//$mail->addAddress('penerima2@contoh.com');
+//$mail->addAddress('penerima3@contoh.com');
+
+// Menambahkan cc atau bcc 
+//$mail->addCC('cc@contoh.com');
+//$mail->addBCC('bcc@contoh.com');
+
+// Subjek email
+$mail->Subject = 'Kirim Email via SMTP Server di PHP menggunakan PHPMailer';
+
+// Mengatur format email ke HTML
+$mail->isHTML(true);
+
+// Konten/isi email
+$mailContent = "<h1>Mengirim Email HTML menggunakan SMTP di PHP</h1>
+    <p>Ini adalah email percobaan yang dikirim menggunakan email server SMTP dengan PHPMailer.</p>";
+$mail->Body = $mailContent;
+
+// Menambahakn lampiran
+//$mail->addAttachment('lmp/file1.pdf');
+//$mail->addAttachment('lmp/file2.png', 'nama-baru-file2.png'); //atur nama baru
+
+// Kirim email
+if(!$mail->send()){
+    echo 'Pesan tidak dapat dikirim.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}else{
+    echo 'Pesan telah terkirim';
+}
